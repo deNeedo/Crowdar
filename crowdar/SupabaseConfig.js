c4d3582100f2cb5bb805e9ee99ba93b261452864
Stanislaw Pilch <spilch@student.agh.edu.pl>
2023-11-12 20:44:27 +0100

    added crucial import

--- crowdar/SupabaseConfig.js
+++ crowdar/SupabaseConfig.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import 'react-native-url-polyfill/auto';
 
 const supabaseUrl = 'https://supabase.crowdar.pl/rest/v1/';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNjk5NzQzNjAwLAogICJleHAiOiAxODU3NTk2NDAwCn0.l3qCfIkFulquRx3UvVP__yz8uCTzSXEG71dFDdkhwZc';
