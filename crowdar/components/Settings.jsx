489f1fa43322acda4b0ba384ffd4d727698540b8
Stanislaw Pilch <stanislawpilch2002@gmail.com>
2024-01-13 19:34:51 +0100

    Settings Buttons

--- crowdar/components/Settings.jsx
+++ crowdar/components/Settings.jsx
@@ -3,7 +3,7 @@ import { View, TouchableOpacity, Text } from 'react-native';
 import { supabase } from '../supabaseClient';
 import AuthContext from './AuthContext';
 
-import { Styles } from '../styles/welcomeScreenStyle';
+import { Styles } from '../styles/SettingsStyle';
 
 export default function NotificationArea({navigation}) {
 
@@ -17,12 +17,12 @@ export default function NotificationArea({navigation}) {
 
     return (
         <View>
-            <Text> Settings Panel </Text>
+            <Text style = {Styles.main_text}> Settings Panel </Text>
             <TouchableOpacity style={Styles.button} onPress={() => navigation.navigate('Account')}>
-                <Text> Account Information </Text>
+                <Text style = {Styles.text_style}> Account Information </Text>
             </TouchableOpacity>
             <TouchableOpacity style={Styles.button} onPress={signOut}>
-                <Text> LOG OUT </Text>
+                <Text style = {Styles.text_style}> LOG OUT </Text>
             </TouchableOpacity>
         </View>
     )
--- crowdar/styles/SettingsStyle.jsx
+++ crowdar/styles/SettingsStyle.jsx
@@ -6,9 +6,36 @@ import {Spacing} from '../constants/Spacing';
 export const Styles = StyleSheet.create({
     text_style: {
         fontFamily: 'Poppins_600SemiBold',
+        fontFamily: 'Poppins_600SemiBold',
                             color: Colors.White,
                             textTransform: 'uppercase',
                             textAlign: 'center',
                             fontSize: Sizes.Medium,
-    }
+    },
+
+    main_text: {
+        fontFamily: 'Poppins_600SemiBold',
+                            color: Colors.Black,
+                            textTransform: 'uppercase',
+                            textAlign: 'center',
+                            fontSize: Sizes.Medium,
+    },
+
+    button: {
+        paddingVertical: 15,
+        paddingHorizontal: 20,
+        width: '45%',
+        borderRadius: 10,
+        margin: 10,
+        padding: Spacing.xSmall,
+        backgroundColor: Colors.Primary,
+        marginVertical: Spacing.Small,
+        shadowColor: Colors.Primary,
+        shadowOffset: {
+            width: 0,
+            height: 10,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 10
+    },
 })
