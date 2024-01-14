1e2f329460a42145bb1f6d30818203b55de90894
deNeedo <jkudrys@protonmail.com>
2024-01-14 13:40:14 +0100

    Updated location sharing process

--- crowdar/components/Home.jsx
+++ crowdar/components/Home.jsx
@@ -1,10 +1,11 @@
 // react main features
 import React, { useState, useContext, useEffect } from 'react';
 import { supabase } from '../supabaseClient';
-import { Alert, View, TouchableOpacity, Text, ImageBackground, Dimensions, FlatList, TextInput } from 'react-native';
+import { Alert, View, TouchableOpacity, Text, ImageBackground, Dimensions, FlatList, TextInput, PermissionsAndroid} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 // connection and authentication
 import AuthContext from './AuthContext';
+import Geolocation from 'react-native-geolocation-service';
 // styling
 import { Styles } from '../styles/welcomeScreenStyle';
 import { Colors } from '../constants/Colors';
@@ -15,12 +16,75 @@ const { height, width } = Dimensions.get("window");
 
 export default function Home({navigation}) {
 	const {session, setSession} = useContext(AuthContext); 
-	const [ friends, setFriends ] = useState([]);
+	const [friends, setFriends] = useState([]);
+	const [locationSharing, setLocationSharing] = useState(false);
+
+	useEffect(() => {fetchFriends();}, [session, friends])
+	useEffect(() => {
+		let intervalId;
+		if (locationSharing) {intervalId = setInterval(setLocation, 10000)}
+		else {clearInterval(intervalId); clearLocation()}
+		return () => clearInterval(intervalId);
+	}, [locationSharing]);
+
+	const requestLocationPermission = async () => {
+		try {
+			const granted = await PermissionsAndroid.request(
+			PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+			{
+				title: 'Geolocation Permission',
+				message: 'Can we access your location?',
+				buttonNeutral: 'Ask Me Later',
+				buttonNegative: 'Cancel',
+				buttonPositive: 'OK',
+			},
+			);
+		   
+			if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (err) {
+			return false;
+		}
+	};
+
+	const setLocation = async () => {
+		const {data} = await supabase.from('profiles').select('username').eq('id', session.user.id);
+		if (data[0].username == null) {Alert.alert("Please specify username in user panel before using the location feature!");}
+		else {
+			const result = requestLocationPermission();
+			result.then(res => {
+				if (res) {
+					Geolocation.getCurrentPosition(
+						async (position) => {
+							await supabase.from('profiles').update({location: position.coords}).eq('id', session.user.id);
+						},
+						(error) => {console.log(error)},
+						{enableHighAccuracy: true, timeout: 10000}
+					);
+				}
+			});
+		}
+    };
+
+	const clearLocation = async () => {
+		if (session != null) {
+			if (session.user != null) {
+				await supabase.from('profiles').update({location: null}).eq('id', session.user.id);
+			}
+		}
+	}
 
 	const fetchFriends = async () => {
-		const { data, error } = await supabase.from('profiles').select('friends').eq('id', session.user.id);
-		if (error) {setFriends([]);}
-		else {setFriends(data[0].friends);}
+		if (session != null) {
+			if (session.user != null) {
+				const {data, error} = await supabase.from('profiles').select('friends').eq('id', session.user.id);
+				if (error) {setFriends([]);}
+				else {setFriends(data[0].friends);}
+			}
+		}
 	}
 
 	//////////SEARCH BAR///////////
@@ -30,7 +94,7 @@ export default function Home({navigation}) {
 		const { data } = await supabase.from('profiles').select('friends').eq('id', session.user.id)
 		let temp_arr = []; let action = true;
 		if (data[0].friends != null) {temp_arr = [...data[0].friends];} 
-		temp_arr.forEach((friend) => {console.log(friend); if (username == friend) {action = false}})
+		temp_arr.forEach((friend) => {if (username == friend) {action = false}})
 		if (action) {
 			temp_arr.push(username);
 			await supabase.from('profiles').update({friends: temp_arr}).eq('id', session.user.id)
@@ -129,15 +193,17 @@ export default function Home({navigation}) {
 				</View>
 
 				<View>
-					<TouchableOpacity style={Styles.button} onPress={fetchFriends}>
-						<Text style = {Styles.text_style}> GetFriends </Text>
+					<TouchableOpacity style={Styles.button} onPress={() => {!locationSharing ? setLocationSharing(true) : setLocationSharing(false)}}>
+						<Text style = {Styles.text_style}> {locationSharing ? 'STOP SHARING LOCATION' : 'START SHARING LOCATION'} </Text>
 					</TouchableOpacity>
+				</View>
 
+				<View >
 					<FlatList
 						data={friends}
 						renderItem={(friend) => {
 							return (
-								<TouchableOpacity style={Styles.button} onPress={() => navigation.navigate('Location', {userId: friend.item})}>
+								<TouchableOpacity style={Styles.button} onPress={() => navigation.navigate('Location', {friendName: friend.item, })}>
 									<Text> {friend.item} </Text>
 								</TouchableOpacity>
 							)
--- crowdar/components/Location.jsx
+++ crowdar/components/Location.jsx
@@ -1,18 +1,18 @@
-import React, { useState, useEffect, useContext, TouchableOpacity } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { supabase } from '../supabaseClient';
 import MapView, { Marker } from 'react-native-maps';
-import { Alert, StyleSheet, View, Text, Button, PermissionsAndroid, Dimensions } from 'react-native';
-import Geolocation from 'react-native-geolocation-service';
+import { Alert, StyleSheet, View, Text, Button, Dimensions } from 'react-native';
 import AuthContext from './AuthContext';
 
 export default function Location({route, navigation}) {
-    
-    // chcecking if the session exists
     const { session, setSession } = useContext(AuthContext);
     const [ location, setLocation] = useState(null);
-    const { userId } = route.params;
+    const { friendName } = route.params;
 
-    // useEffect(() => {console.log(location)}, [location]);
+	useEffect(() => {
+		let intervalId = setInterval(getLocation, 10000)
+		return () => clearInterval(intervalId);
+	}, []);
 
     useEffect(() => {
         const checkSession = async () => {
@@ -24,82 +24,27 @@ export default function Location({route, navigation}) {
         checkSession();
     }, [session, navigation]);
 
-    // signout
-    const signOut = async () => {
-        await supabase.auth.signOut();
-
-        setSession(null);
-        navigation.navigate('Login');
-    };
-    
-    // state to hold location
-    // const [location, setLocation] = useState(false);
-    // function to check permissions and get Location
     const getLocation = async () => {
         const {data} = await supabase.from('profiles').select('username').eq('id', session.user.id);
         if (data[0].username == null) {Alert.alert("Please specify username in user panel before using the location feature!");}
         else {
-            const result = requestLocationPermission();
-            result.then(res => {
-            if (res) {
-                Geolocation.getCurrentPosition(
-                    async (position) => {
-                        await supabase.from('profiles').update({location: position.coords}).eq('id', session.user.id);
-                        const {data} = await supabase.from('profiles').select('location').eq('username', userId);
-                        if (data[0].location == null) {setLocation(null);}
-                        else {setLocation(data[0].location);}
-                    },
-                    (error) => {setLocation(null);},
-                    {enableHighAccuracy: true, timeout: 30000}
-                );
-            }
-            });
+            const {data} = await supabase.from('profiles').select('location').eq('username', friendName);
+            if (data[0].location == null) {setLocation(null);}
+            else {setLocation(data[0].location);}
         }
     };
     return (
-        <View /*style={styles.container}*/ style={{marginTop: 50, height: height / 2.5}} resizeMode='contain'>
-            <Text>Welcome!</Text>
-            <View style={{marginTop: 10, padding: 10, borderRadius: 10, width: '30%'}}>
-                <Button title="Get Location" onPress={getLocation} />
-            </View>
-            <Text> Latitude: {location ? location.latitude : null} </Text>
-            <Text> Longitude: {location ? location.longitude : null} </Text>
-            <View style={{marginTop: 10, padding: 10, borderRadius: 10, width: '30%'}}>
-                {location ?
-                    <MapView style={{height: height / 2, width: width}} showsUserLocation={true}>
-                        <Marker coordinate={{latitude: location.latitude, longitude: location.longitude}}/>
-                    </MapView> : <Text> Friend is not sharing their location! </Text>
-                }
-            </View>
-            <View>
-                <Button title="Log Out" onPress={signOut}> </Button>
-            </View>
+        <View resizeMode='contain'>
+            {location ?
+                <MapView style={{height: height, width: width}} showsUserLocation={true}>
+                    <Marker coordinate={{latitude: location.latitude, longitude: location.longitude}}/>
+                </MapView> : <Text> Friend is not sharing their location! </Text>
+            }
         </View>
     );
 };
+
 const { height, width } = Dimensions.get("window");
-const requestLocationPermission = async () => {
-    try {
-        const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-        {
-            title: 'Geolocation Permission',
-            message: 'Can we access your location?',
-            buttonNeutral: 'Ask Me Later',
-            buttonNegative: 'Cancel',
-            buttonPositive: 'OK',
-        },
-        );
-       
-        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-            return true;
-        } else {
-            return false;
-        }
-    } catch (err) {
-        return false;
-    }
-};
 
 const styles = StyleSheet.create({
 container: {
